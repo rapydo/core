d2782cbf7c038ec0e44f3abb9b769bf864cdf0de
Mattia D'Antonio <m.dantonio@cineca.it>
2017-09-28 14:10:48 +0200

    Test fix

--- projects/neo/backend/apis/neo_tests_endpoints.py
+++ projects/neo/backend/apis/neo_tests_endpoints.py
@@ -4,7 +4,7 @@
 An endpoint example
 """
 
-from flask import current_app
+# from flask import current_app
 from restapi.rest.definition import EndpointResource
 # from restapi.services.detect import SQL_AVAILABLE, GRAPHDB_AVAILABLE
 
@@ -12,13 +12,14 @@ from utilities.logs import get_logger
 
 log = get_logger(__name__)
 
-if current_app.config['TESTING']:
-    class DoTests(EndpointResource):
 
-        def get(self):
+# if current_app.config['TESTING']:
+class DoTests(EndpointResource):
 
-            graph = self.global_get_service('neo4j')
-            print(graph)
-            log.warning("a call")
-            graph.cypher("MATCH (n) RETURN n")
-            return self.force_response('Hello world')
+    def get(self):
+
+        graph = self.global_get_service('neo4j')
+        print(graph)
+        log.warning("a call")
+        graph.cypher("MATCH (n) RETURN n")
+        return self.force_response('Hello world')
--- projects/sql/backend/apis/sql_tests_endpoints.py
+++ projects/sql/backend/apis/sql_tests_endpoints.py
@@ -4,7 +4,7 @@
 An endpoint example
 """
 
-from flask import current_app
+# from flask import current_app
 from restapi.rest.definition import EndpointResource
 # from restapi.services.detect import SQL_AVAILABLE, GRAPHDB_AVAILABLE
 
@@ -13,11 +13,11 @@ from utilities.logs import get_logger
 log = get_logger(__name__)
 
 
-if current_app.config['TESTING']:
-    class SqlEndPoint(EndpointResource):
+# if current_app.config['TESTING']:
+class SqlEndPoint(EndpointResource):
 
-        def get(self):
-            sql = self.global_get_service('sql')
-            print(sql)
-            log.warning("a call")
-            return self.force_response('Hello world!')
+    def get(self):
+        sql = self.global_get_service('sql')
+        print(sql)
+        log.warning("a call")
+        return self.force_response('Hello world!')

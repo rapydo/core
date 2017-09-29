4dad83d31e9d73cb0a0f2e89379305583bfab7bd
Mattia D'Antonio <m.dantonio@cineca.it>
2017-09-29 07:25:27 +0200

    Bug fix for tests

--- projects/neo/backend/apis/neo_tests_endpoints.py
+++ projects/neo/backend/apis/neo_tests_endpoints.py
@@ -6,7 +6,7 @@ An endpoint example
 
 # from flask import current_app
 from restapi.rest.definition import EndpointResource
-
+from utilities.meta import Meta
 from utilities.logs import get_logger
 
 log = get_logger(__name__)
@@ -15,10 +15,17 @@ log = get_logger(__name__)
 # if current_app.config['TESTING']:
 class DoTests(EndpointResource):
 
-    def get(self):
+    def test_1(self):
+        print("Mi hai trovato!!")
+
+    def get(self, test_num):
 
         graph = self.get_service_instance('neo4j')
         print(graph)
         log.warning("a call")
         graph.cypher("MATCH (n) RETURN n")
+
+        meta = Meta()
+        methods = meta.get_methods_inside_instance(self)
+        print(methods)
         return self.force_response('Hello world')

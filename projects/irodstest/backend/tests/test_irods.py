8a34c69a7a0072fb75d54846cdaa4b0556a3d180
Mattia D'Antonio <m.dantonio@cineca.it>
2017-10-23 09:47:32 +0200

    Let's test irods

--- projects/irodstest/backend/tests/test_irods.py
+++ projects/irodstest/backend/tests/test_irods.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import os
 from restapi.tests import BaseTests
 from restapi.tests.utilities import API_URI
 from restapi.services.detect import detector
@@ -24,3 +25,54 @@ class TestApp(BaseTests):
         home = irods.get_user_home()
 
         assert home == "/tempZone/home/irods"
+
+        path = irods.get_absolute_path(home.split("/"))
+        assert path == home
+
+        data_obj = os.path.join(path, "test.txt")
+        collection = os.path.join(path, "sub")
+        collection2 = os.path.join(path, "sub2")
+        data_obj2 = os.path.join(collection, "test2.txt")
+        data_obj3 = os.path.join(collection, "test3.txt")
+
+        collection = irods.get_collection_from_path(data_obj)
+        assert collection == path
+
+        assert irods.exists(path)
+        assert not irods.exists(data_obj)
+
+        assert irods.is_collection(path)
+        assert not irods.is_collection(data_obj)
+
+        assert not irods.is_dataobject(path)
+        assert not irods.is_dataobject(data_obj)
+
+        sub_path = irods.getPath(path, "/tempZone")
+        assert sub_path == "home/irods"
+
+        irods.create_empty(collection, directory=True)
+        irods.create_empty(data_obj)
+
+        assert irods.exists(collection)
+        assert irods.exists(data_obj)
+
+        assert irods.is_collection(collection)
+        assert not irods.is_collection(data_obj)
+
+        assert not irods.is_dataobject(collection)
+        assert irods.is_dataobject(data_obj)
+
+        irods.copy(data_obj, data_obj2)
+        irods.move(data_obj2, data_obj3)
+        irods.copy(collection, collection2, recursive=True)
+
+        content = irods.list(path)
+        assert content == {}
+
+        irods.remove(data_obj3)
+        content = irods.list(path)
+        assert content == {}
+
+        irods.remove(collection2, recursive=True)
+        content = irods.list(path)
+        assert content == {}

4c7e3b8072ad5b602906e722a39872b90a67c8dc
Mattia <mdantonio@users.noreply.github.com>
2018-04-18 09:18:27 +0200

    0.6.1 (#10)
    * Upgrade to rapydo 0.6.1
    
    * Added empty package.json for template projects
    
    * Added empty custom.ts for template projects
    
    * Added default app.js for template projects
    
    * Fixed app.js path
    
    * Added default style.css for template projects
    
    * Removed old bower.json from template projects
    
    * Fix template projects
    
    * Restored frontend libs
    
    * Adding missing template files
    
    * Template update
    
    * Template upgrade to be compliant to 0.6.1 requirements

---
+++ projects/basic/frontend/js/app.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular.module('web', [
+        'ngSanitize',
+        'ngAnimate',
+        'ngMessages',
+        'ui.router',
+        'ui.bootstrap',
+        'satellizer',
+        'formly',
+        'formlyBootstrap',
+/*        'angular.chips',*/
+        'tableSort',
+/*        'flow',*/
+        'ngFileSaver'
+    ]);
+
+})();

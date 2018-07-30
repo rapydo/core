5e5c37caf0b259e6576f17022392cd5dd9d5dab4
Mattia <mdantonio@users.noreply.github.com>
2018-07-30 09:20:07 +0200

    0.6.2 release (#12)
    * Upgrade to rapydo 0.6.1
    
    * Added empty package.json for template projects
    
    * Added empty custom.ts for template projects
    
    * Added default app.js for template projects
    
    * Fixed app.js path
    
    * Added default style.css for template projects
    
    * Removed old bower.json from template projects
    
    * Fix template projects
    
    * Restored frontend libs
    
    * Adding missing template files
    
    * Template update
    
    * Template upgrade to be compliant to 0.6.1 requirements
    
    * Preparing new version
    
    * Added missing template files
    
    * Version upgrade
    
    * Removed unused requirements file
    
    * Fixed version in image FROM
    
    * Fix template files for frontend
    
    * Bug fix to connect api to celery backend
    
    * Configuring MongoDB as celery backend
    
    * Added update_state to celery task to test celery backend
    
    * Fix new frontend enabling syntax
    
    * Adding new required files to template projects
    
    * Fix irods configuration to use the right backend

--- projects/celerytest/backend/tasks/test_task.py
+++ projects/celerytest/backend/tasks/test_task.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from restapi.flask_ext.flask_celery import CeleryExt
+import time
 
 from utilities.logs import get_logger
 log = get_logger(__name__)
@@ -15,6 +16,17 @@ def testme(self):
     with celery_app.app.app_context():
 
         # self.db = celery_app.get_service('sqlalchemy')
+
+        log.info("Task started!")
+
+        self.update_state(state="STARTING", meta={'current': 1, 'total': 3})
+        time.sleep(5)
+
+        self.update_state(state="COMPUTING", meta={'current': 2, 'total': 3})
+        time.sleep(5)
+
+        self.update_state(state="FINAL", meta={'current': 3, 'total': 3})
+        time.sleep(5)
 
         log.info("Task executed!")
         return "Task executed!"

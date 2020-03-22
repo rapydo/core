3779b92430484c8c75fa6c99508da88f84a66977
Mattia <m.dantonio81@gmail.com>
2020-03-22 15:49:08 +0100

    Added project based on old wrapped response

---
+++ projects/wrapper_response/backend/tests/test_sql.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from restapi.tests import BaseTests, API_URI
+from restapi.utilities.htmlcodes import hcodes
+# from restapi.utilities.logs import log
+
+
+class TestApp(BaseTests):
+
+    def test_01_x(self, client):
+
+        endpoint = API_URI + '/tests/1'
+        r = client.get(endpoint)
+        assert r.status_code == hcodes.HTTP_OK_BASIC

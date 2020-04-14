4d84155b08d2071e45dd08a0fb92902ed9ff8588
Mattia <m.dantonio81@gmail.com>
2020-04-14 08:08:52 +0200

    Added marshal tests

--- projects/apispecs/backend/apis/apispecs_tests_endpoints.py
+++ projects/apispecs/backend/apis/apispecs_tests_endpoints.py
@@ -9,13 +9,14 @@ from flask_apispec import MethodResource
 # from flask_apispec import Ref
 from marshmallow import Schema, fields, validate
 
+from restapi.utilities.htmlcodes import hcodes
 from restapi.utilities.logs import log
 
 
 class TestNum(Schema):
     test_num = fields.Int(
         required=True,
-        validate=validate.Range(min=1, max=2)
+        validate=validate.Range(min=1, max=3)
     )
 
 
@@ -85,8 +86,8 @@ class MarshalResponses(MethodResource, EndpointResource):
     }
 
     @use_kwargs(TestNum)
-    @marshal_with(Error, code=400)
-    # @marshal_with(OutSchema, code=200)
+    @marshal_with(Error, code=404)
+    @marshal_with(OutSchema, code=200)
     @decorators.catch_errors()
     def get(self, **kwargs):
 
@@ -96,15 +97,15 @@ class MarshalResponses(MethodResource, EndpointResource):
 
         test_num = kwargs.get("test_num")
         if test_num == 1:
-            return self.response("1")
+            data = {"value": "123", "hideme": "Not returned"}
+            return self.response(data)
 
         if test_num == 2:
-            raise RestApiException("Just an error")
+            error = {"mykey": "Just an error", "hideme": "Not returned"}
+            raise RestApiException(error)
 
-        log.info(kwargs)
+        if test_num == 3:
+            error = {"mykey": "Just an error", "hideme": "Not returned"}
+            raise RestApiException(error, status_code=hcodes.HTTP_BAD_REQUEST)
 
-        # return self.response("blabla")
-        data = {"value": "123", "xyz": "abc"}
-
-        raise RestApiException(data)
-        return self.response(data)
+        raise RestApiException("Test {} not implemented".format(test_num))
--- projects/apispecs/backend/tests/test_apispecs.py
+++ projects/apispecs/backend/tests/test_apispecs.py
@@ -10,12 +10,37 @@ class TestApp(BaseTests):
     def test_01_x(self, client):
 
         log.debug("Executing tests from {}", self.__class__.__module__)
-        endpoint = API_URI + '/tests'
+        endpoint = API_URI + '/flat'
 
-        r = client.get(endpoint, "", {"test_num": 1})
+        r = client.get(endpoint, data={"test_num": 1})
         assert r.status_code == hcodes.HTTP_OK_BASIC
         assert self.get_content(r) == "1"
 
-        r = client.get(endpoint, "", {"test_num": 2})
+        r = client.get(endpoint, data={"test_num": 2})
         assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
         assert self.get_content(r) == "Just an error"
+
+        endpoint = API_URI + '/marshal'
+
+        r = client.get(endpoint, data={"test_num": 1})
+        assert r.status_code == hcodes.HTTP_OK_BASIC
+        c = self.get_content(r)
+        assert "value" in c
+        assert "hideme" not in c
+        assert c["value"] == 123
+
+        r = client.get(endpoint, data={"test_num": 2})
+        assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
+        c = self.get_content(r)
+        assert "mykey" in c
+        # marshal removed this key from the response
+        assert "hideme" not in c
+        assert c["mykey"] == "Just an error"
+
+        r = client.get(endpoint, data={"test_num": 3})
+        assert r.status_code == hcodes.HTTP_BAD_REQUEST
+        c = self.get_content(r)
+        assert "mykey" in c
+        # no marshal applied
+        assert "hideme" in c
+        assert c["mykey"] == "Just an error"

08f37e84d6cab226b7b157085961712e02eb35c1
Mattia <m.dantonio81@gmail.com>
2020-04-19 15:20:50 +0200

    Added auth required to marshal /data POC, remove /flat and /marshal basic tests

--- projects/apispecs/backend/apis/apispecs_tests_endpoints.py
+++ projects/apispecs/backend/apis/apispecs_tests_endpoints.py
@@ -5,14 +5,15 @@ from marshmallow import fields, validate
 from flask_apispec import use_kwargs
 from flask_apispec import marshal_with
 from flask_apispec import MethodResource
+from flask_apispec.annotations import doc
 
 from restapi.models import Schema
 from restapi.services.detect import detector
 from restapi.rest.definition import EndpointResource
 from restapi.exceptions import RestApiException
 from restapi import decorators
-from restapi.utilities.htmlcodes import hcodes
-from restapi.utilities.logs import log
+# from restapi.utilities.htmlcodes import hcodes
+# from restapi.utilities.logs import log
 
 from apispecs.models.neo4j import BLOOD_TYPES
 
@@ -107,8 +108,14 @@ class MarshalData(MethodResource, EndpointResource):
         }
     }
 
+    @doc(responses={"200": {"description": "Endpoint is working"}})
+    @doc(
+        summary="Experiments with ApiSpec",
+        description="Proof of concept for ApiSpec integration in RAPyDo"
+    )
     @marshal_with(OutputSchema(many=True), code=200)
     @decorators.catch_errors()
+    @decorators.auth.required()
     def get(self, **kwargs):
 
         graph = self.get_service_instance('neo4j')
@@ -123,6 +130,7 @@ class MarshalData(MethodResource, EndpointResource):
 
     @use_kwargs(InputSchema())
     @decorators.catch_errors()
+    @decorators.auth.required()
     def post(self, **kwargs):
 
         graph = self.get_service_instance('neo4j')
@@ -136,6 +144,7 @@ class MarshalData(MethodResource, EndpointResource):
 
     @use_kwargs(InputSchema(strip_required=True, exclude=("email",)))
     @decorators.catch_errors()
+    @decorators.auth.required()
     def put(self, uuid, **kwargs):
 
         graph = self.get_service_instance('neo4j')
@@ -152,6 +161,7 @@ class MarshalData(MethodResource, EndpointResource):
         return self.empty_response()
 
     @decorators.catch_errors()
+    @decorators.auth.required()
     def delete(self, uuid):
 
         graph = self.get_service_instance('neo4j')
@@ -164,86 +174,3 @@ class MarshalData(MethodResource, EndpointResource):
         d.delete()
 
         return self.empty_response()
-
-
-class TestNum(Schema):
-    test_num = fields.Int(
-        required=True,
-        validate=validate.Range(min=1, max=3)
-    )
-
-
-class OutSchema(Schema):
-    value = fields.Int()
-
-
-class Error(Schema):
-    mykey = fields.Str()
-
-
-class MarshalResponses(MethodResource, EndpointResource):
-
-    labels = ['helpers']
-
-    _GET = {
-        "/marshal": {
-            "summary": "Experiments with ApiSpec",
-            "description": "Proof of concept for ApiSpec integration in RAPyDo",
-            "responses": {"200": {"description": "Endpoint is working"}},
-        }
-    }
-
-    @use_kwargs(TestNum)
-    @marshal_with(OutSchema, code=200)
-    @marshal_with(Error, code=404)
-    @decorators.catch_errors()
-    def get(self, **kwargs):
-
-        graph = self.get_service_instance('neo4j')
-
-        log.debug(graph)
-
-        test_num = kwargs.get("test_num")
-        if test_num == 1:
-            data = {"value": "123", "hideme": "Not returned"}
-            return self.response(data)
-
-        if test_num == 2:
-            error = {"mykey": "Just an error", "hideme": "Not returned"}
-            raise RestApiException(error, status_code=hcodes.HTTP_BAD_NOTFOUND)
-
-        if test_num == 3:
-            error = {"mykey": "Just an error", "hideme": "Not returned"}
-            raise RestApiException(error, status_code=hcodes.HTTP_BAD_REQUEST)
-
-        raise RestApiException("Test {} not implemented".format(test_num))
-
-
-class FlatResponses(MethodResource, EndpointResource):
-
-    labels = ['helpers']
-
-    _GET = {
-        "/flat": {
-            "summary": "Experiments with ApiSpec",
-            "description": "Proof of concept for ApiSpec integration in RAPyDo",
-            "responses": {"200": {"description": "Endpoint is working"}},
-        }
-    }
-
-    @use_kwargs(TestNum)
-    @decorators.catch_errors()
-    def get(self, **kwargs):
-
-        graph = self.get_service_instance('neo4j')
-
-        log.debug(graph)
-
-        test_num = kwargs.get("test_num")
-        if test_num == 1:
-            return self.response("1")
-
-        if test_num == 2:
-            raise RestApiException("Just an error")
-
-        raise RestApiException("Test {} not implemented".format(test_num))
--- projects/apispecs/backend/tests/test_apispecs.py
+++ projects/apispecs/backend/tests/test_apispecs.py
@@ -10,52 +10,6 @@ class TestApp(BaseTests):
     def test_01_x(self, client):
 
         log.debug("Executing tests from {}", self.__class__.__module__)
-        endpoint = API_URI + '/flat'
-
-        r = client.get(endpoint)
-        assert r.status_code == hcodes.HTTP_BAD_REQUEST
-        c = self.get_content(r)
-        assert "test_num" in c
-        assert c["test_num"][0] == "Missing data for required field."
-
-        r = client.get(endpoint, data={"test_num": 1})
-        assert r.status_code == hcodes.HTTP_OK_BASIC
-        assert self.get_content(r) == "1"
-
-        r = client.get(endpoint, data={"test_num": 2})
-        assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
-        assert self.get_content(r) == "Just an error"
-
-        endpoint = API_URI + '/marshal'
-
-        r = client.get(endpoint)
-        assert r.status_code == hcodes.HTTP_BAD_REQUEST
-        c = self.get_content(r)
-        assert "test_num" in c
-        assert c["test_num"][0] == "Missing data for required field."
-
-        r = client.get(endpoint, data={"test_num": 1})
-        assert r.status_code == hcodes.HTTP_OK_BASIC
-        c = self.get_content(r)
-        assert "value" in c
-        assert "hideme" not in c
-        assert c["value"] == 123
-
-        r = client.get(endpoint, data={"test_num": 2})
-        assert r.status_code == hcodes.HTTP_BAD_NOTFOUND
-        c = self.get_content(r)
-        assert "mykey" in c
-        # marshal removed this key from the response
-        assert "hideme" not in c
-        assert c["mykey"] == "Just an error"
-
-        r = client.get(endpoint, data={"test_num": 3})
-        assert r.status_code == hcodes.HTTP_BAD_REQUEST
-        c = self.get_content(r)
-        assert "mykey" in c
-        # no marshal applied
-        assert "hideme" in c
-        assert c["mykey"] == "Just an error"
 
         # Starting from an empty list
         endpoint = API_URI + '/data'
